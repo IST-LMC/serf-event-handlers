0750664637fd5dfae7963d12ec688cf2d94148c0
Dustin Lactin <dustin.lactin@gmail.com>
2018-01-05 22:33:07 +0000

    Fixed indentation in python event handler

--- haproxy_handler.py
+++ haproxy_handler.py
@@ -12,11 +12,10 @@ event_array = []
 for membership_event in membership_events:
   event_array = membership_event.split('\t')
   event = { 'hostname': event_array[0], 'ipaddress': event_array[1], 'role': event_array[2], 'state': os.environ['SERF_EVENT'] }
-
-if event['role'] == 'app' and re.match('.*-as-app.*$', event['hostname']):
-  if event['state'] == 'member-join':
-    join.register(event['ipaddress'])
-  elif event['state'] == 'member-leave':
-    leave.unregister(event['ipaddress'])
-  elif event['state'] == 'member-failed':
-    leave.unregister(event['ipaddress'])
+  if event['role'] == 'app' and re.match('.*-as-app.*$', event['hostname']):
+    if event['state'] == 'member-join':
+      join.register(event['ipaddress'])
+    elif event['state'] == 'member-leave':
+      leave.unregister(event['ipaddress'])
+    elif event['state'] == 'member-failed':
+      leave.unregister(event['ipaddress'])
