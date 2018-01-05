d36c0e83df277ad37fdeb2aa7f27740121920701
Dustin Lactin <dustin.lactin@gmail.com>
2018-01-05 17:55:58 +0000

    Updated handler to work with multiple events

--- haproxy_handler.rb
+++ haproxy_handler.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 
-membership_event = $stdin.read.split(/\t/)
+membership_events = $stdin.readlines.each do |membership_event|
+  event_array = membership_event.split(/\t/)
 
-event = { :hostname => membership_event[0], :ipaddress => membership_event[1], :role => membership_event[2], :state => ENV['SERF_EVENT'] }
+  event = { :hostname => event_array[0], :ipaddress => event_array[1], :role => event_array[2], :state => ENV['SERF_EVENT'] }
 
-if event[:role] === 'app' then
-  case event[:state]
-  when 'member-join'
-    %x(/usr/local/bin/register_app_node.py #{event[:ipaddress]})
-  when 'member-leave'
-    puts "Drain and remove node from load balancer"
-  when 'member-failed'
-    puts "Drain and remove node from load balancer"
+  if event[:role] === 'app' && event[:hostname] =~ /^.*-as-app.*$/ then
+    case event[:state]
+    when 'member-join'
+      %x(/usr/local/bin/register_app_node.py #{event[:ipaddress]})
+    when 'member-leave'
+      puts "Drain and remove node from load balancer"
+    when 'member-failed'
+      puts "Drain and remove node from load balancer"
+    end
   end
 end
