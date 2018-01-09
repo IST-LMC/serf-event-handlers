9e1f2b682ac37f601e40f315d15a7330dc6a0d5e
Dustin Lactin <dustin.lactin@gmail.com>
2018-01-09 15:40:37 +0000

    Added drain event with lifecycle completion

--- haproxy_handler.py
+++ haproxy_handler.py
@@ -10,8 +10,20 @@ membership_events = sys.stdin.readlines()
 event_array = []
 
 for membership_event in membership_events:
-  event_array = membership_event.split('\t')
+  if os.environ['SERF_EVENT'] == 'user':
+    event_array = membership_event.split(':')
+  else:
+    event_array = membership_event.split('\t')
+
   event = { 'hostname': event_array[0], 'ipaddress': event_array[1], 'role': event_array[2], 'state': os.environ['SERF_EVENT'] }
+  try:
+    event['hookname'] = event_array[3]
+    event['groupname'] = event_array[4]
+    event['actiontoken'] = event_array[5]
+    event['instance_id'] = event_array[6]
+  except:
+    print("No Lifecycle Hook Info")
+
   if event['role'] == 'app' and re.match('.*-as-app.*$', event['hostname']):
     if event['state'] == 'member-join':
       join.register(event['ipaddress'])
@@ -19,3 +31,7 @@ for membership_event in membership_events:
       leave.unregister(event['ipaddress'])
     elif event['state'] == 'member-failed':
       leave.unregister(event['ipaddress'])
+    elif event['state'] == 'member-reap':
+      print("% reaped." % event['hostname'])
+  elif os.environ['SERF_USER_EVENT'] == 'drain':
+    leave.drain(event)
